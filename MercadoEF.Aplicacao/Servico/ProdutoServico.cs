98df7c887b90e5cc213dfe75bc85d964f8d3044f
souzavini <45321382+souzavini@users.noreply.github.com>
2020-08-04 09:35:35 -0300

    Add files via upload

---
+++ MercadoEF.Aplicacao/Servico/ProdutoServico.cs
@@ -0,0 +1,65 @@
+﻿using MercadoEF.Dominio.Contratos;
+using MercadoEF.Dominio.Contratos.Servicos;
+using MercadoEF.Dominio.Entidades;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MercadoEF.Aplicacao.Servico
+{
+    public class ProdutoServico : IProdutoServico
+    {
+        private readonly IProdutoRepositorio ProdutoRepositorio;
+        public ProdutoServico(IProdutoRepositorio produtoRepositorio)
+        {
+            ProdutoRepositorio = produtoRepositorio;
+        }
+
+        public Task<IEnumerable> MaiorProduto()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Produto> ObterPorId(int id, bool incluirVendas = false)
+        {
+            return ProdutoRepositorio.ObterPorId(id);
+        }
+
+        public Task<IEnumerable> ObterPorNome(string nome, bool incluirVendas = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable> ObterTodos(bool incluirVendas = false)
+        {
+            return await ProdutoRepositorio.ObterTodos();
+        }
+
+        public async Task Adicionar(Produto produto)
+        {
+              ProdutoRepositorio.Adicionar(produto);
+        }
+
+        public async Task<bool> SalvarAlteracoes()
+        {
+            return await ProdutoRepositorio.SalvarAlteracoes();
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            ProdutoRepositorio.Atualizar(produto);
+        }
+
+        public void Remover(Produto produto)
+        {
+            ProdutoRepositorio.Remover(produto);
+        }
+
+        public IEnumerable ObterProdutos()
+        {
+            return ProdutoRepositorio.ObterProdutos();
+        }
+    }
+}
